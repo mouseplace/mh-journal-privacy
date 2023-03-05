352a8d7e2c21e9875b99d0b3cab0bd25a5698a9e
Brad Parbs <brad@bradparbs.com>
2023-03-05 00:00:11 -0600

    1.0.2

--- mh-journal-privacy.user.js
+++ mh-journal-privacy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ Mousehunt - Journal Privacy
-// @version      1.0.1
+// @version      1.0.2
 // @description  Hides usernames from the journal entries on the journal page.
 // @license      MIT
 // @author       bradp
@@ -9,64 +9,11 @@
 // @icon         https://brrad.com/mouse.png
 // @grant        none
 // @run-at       document-end
+// @require      https://cdn.jsdelivr.net/npm/mousehunt-utils@1.2.0/mousehunt-utils.js
 // ==/UserScript==
 
 ((function () {
 	'use strict';
-
-	/**
-	 * Add styles to the page.
-	 *
-	 * @param {string} styles The styles to add.
-	 */
-	const addStyles = (styles) => {
-		const existingStyles = document.getElementById('mh-mouseplace-custom-styles');
-
-		if (existingStyles) {
-			existingStyles.innerHTML += styles;
-			return;
-		}
-
-		const style = document.createElement('style');
-		style.id = 'mh-mouseplace-custom-styles';
-		style.innerHTML = styles;
-		document.head.appendChild(style);
-	};
-
-	/**
-	 * Do something when ajax requests are completed.
-	 *
-	 * @param {Function} callback    The callback to call when an ajax request is completed.
-	 * @param {string}   url         The url to match. If not provided, all ajax requests will be matched.
-	 * @param {boolean}  skipSuccess Skip the success check.
-	 */
-	const onAjaxRequest = (callback, url = null, skipSuccess = false) => {
-		const req = XMLHttpRequest.prototype.open;
-		XMLHttpRequest.prototype.open = function () {
-			this.addEventListener('load', function () {
-				if (this.responseText) {
-					let response = {};
-					try {
-						response = JSON.parse(this.responseText);
-					} catch (e) {
-						return;
-					}
-
-					if (response.success || skipSuccess) {
-						if (! url) {
-							callback(response);
-							return;
-						}
-
-						if (this.responseURL.indexOf(url) !== -1) {
-							callback(response);
-						}
-					}
-				}
-			});
-			req.apply(this, arguments);
-		};
-	};
 
 	const applyClassToNames = () => {
 		const entries = document.querySelectorAll('#journalContainer .entry.relicHunter_start .journaltext');
